c1ff067b0e229bce83deffdea48f388c7b36685d
HyeongJun Yoon <family72020@gmail.com>
2023-03-09 18:42:45 +0900

    Revert "📄 Fix index component"
    This reverts commit 8a29779af7094c78eaf0978ab764e8bcfef0260e.

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
-export default function App() {
+function App() {
   return <div className="App"></div>;
 }
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-const root = document.getElementById('root') as HTMLElement;
-ReactDOM.createRoot(root).render(<App />);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
