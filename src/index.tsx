78352875282f1581dc05773077201336ffb758f4
HyeongJun Yoon <family72020@gmail.com>
2023-03-09 18:45:15 +0900

    📄 Fix index

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,6 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const root = document.getElementById('root') as HTMLElement;
+ReactDOM.createRoot(root).render(<App />);
