d9c47abda6241fd9060eeec08b555b5066d44fe6
HyeongJun Yoon <family72020@gmail.com>
2023-03-11 15:09:17 +0900

    📄 Add Globalstyle

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,12 @@
+import GlobalStyle from './global-styles';
+import { RouterProvider } from 'react-router-dom';
+import router from './Router';
+
 export default function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <GlobalStyle />
+      <RouterProvider router={router} />
+    </div>
+  );
 }
