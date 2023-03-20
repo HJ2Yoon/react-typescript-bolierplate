47794089d960d24e1fa874f175e66ac6e8a55881
HyeongJun Yoon <family72020@gmail.com>
2023-03-20 23:44:36 +0900

    📄 Add Recoil

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from 'react-query';
+import { RecoilRoot } from 'recoil';
 import App from './App';
 import GlobalStyle from './global-styles';
 
@@ -8,8 +9,11 @@ const queryClient = new QueryClient();
 
 const root = document.getElementById('root') as HTMLElement;
 ReactDOM.createRoot(root).render(
-  <QueryClientProvider client={queryClient}>
-    <GlobalStyle />
-    <App />
-  </QueryClientProvider>,
+  <RecoilRoot>
+    <QueryClientProvider client={queryClient}>
+      <GlobalStyle />
+      <App />
+    </QueryClientProvider>
+    ,
+  </RecoilRoot>,
 );
